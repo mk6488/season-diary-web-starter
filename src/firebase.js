95a0901d54e57ba7531459cdc5a2b8a4eb12d1f0
Mike Katholnig <mk3819@gmail.com>
2025-09-07 13:00:48 +0100

    Config: move Firebase config to env-driven module; update README with .env keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCAolG2Ftvid-79V-q9q6My7ZAb03Vicqk",
-    authDomain: "inter-boys.firebaseapp.com",
-    projectId: "inter-boys",
-    storageBucket: "inter-boys.firebasestorage.app",
-    messagingSenderId: "403180048584",
-    appId: "1:403180048584:web:8a0fcb467ea4fa341b55fd"
-};
+import firebaseConfig from './config/firebaseConfig';
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
